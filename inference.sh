8963663a2d031820025773d1341a698d6004428f
dewa <95997298+dewasahu2003@users.noreply.github.com>
2024-06-17 12:41:06 +0000

    changes

--- inference.sh
+++ inference.sh
@@ -1,13 +1,13 @@
 #VITON-HD
 ##paired setting
-accelerate launch --num_processes 2 inference.py --pretrained_model_name_or_path "yisol/IDM-VTON" \
+accelerate launch --num_processes 2 IDM-VTON/inference.py --pretrained_model_name_or_path "yisol/IDM-VTON" \
     --width 768 --height 1024 --num_inference_steps 30 \
     --output_dir "result" --data_dir "IDM-VTON/zalando-hd-resized" \
     --seed 42 --test_batch_size 2 --guidance_scale 2.0
 
 
 ##unpaired setting
-accelerate launch --num_processes 2 inference.py --pretrained_model_name_or_path "yisol/IDM-VTON" \
+accelerate launch --num_processes 2 IDM-VTON/inference.py --pretrained_model_name_or_path "yisol/IDM-VTON" \
     --width 768 --height 1024 --num_inference_steps 30 \
     --output_dir "result" --unpaired --data_dir "IDM-VTON/zalando-hd-resized" \
     --seed 42 --test_batch_size 2 --guidance_scale 2.0
